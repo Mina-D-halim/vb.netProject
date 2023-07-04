f525755ada2f14c4bb29d0fce3034c698bcb50e6
mina dawoud halim <mina.dawoud@onewaygroups.com>
2023-07-04 11:46:50 +0300

    Add project files.

---
+++ learn_vb.net/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_msg = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_datatype = New System.Windows.Forms.Button()
+        Me.btn_mssg = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btn_msg
+        '
+        Me.btn_msg.Location = New System.Drawing.Point(374, 52)
+        Me.btn_msg.Name = "btn_msg"
+        Me.btn_msg.Size = New System.Drawing.Size(212, 65)
+        Me.btn_msg.TabIndex = 0
+        Me.btn_msg.Text = "show msg"
+        Me.btn_msg.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(257, 130)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(79, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(374, 130)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(195, 51)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "varible"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btn_datatype
+        '
+        Me.btn_datatype.Location = New System.Drawing.Point(362, 220)
+        Me.btn_datatype.Name = "btn_datatype"
+        Me.btn_datatype.Size = New System.Drawing.Size(207, 52)
+        Me.btn_datatype.TabIndex = 3
+        Me.btn_datatype.Text = "Varibal data type"
+        Me.btn_datatype.UseVisualStyleBackColor = True
+        '
+        'btn_mssg
+        '
+        Me.btn_mssg.Location = New System.Drawing.Point(62, 302)
+        Me.btn_mssg.Name = "btn_mssg"
+        Me.btn_mssg.Size = New System.Drawing.Size(143, 62)
+        Me.btn_mssg.TabIndex = 4
+        Me.btn_mssg.Text = "textboxin var"
+        Me.btn_mssg.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(87, 119)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(87, 146)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(87, 173)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"mina", "dawoud", "halim", "hakim"})
+        Me.ListBox1.Location = New System.Drawing.Point(639, 98)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 160)
+        Me.ListBox1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btn_mssg)
+        Me.Controls.Add(Me.btn_datatype)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.btn_msg)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_msg As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_datatype As Button
+    Friend WithEvents btn_mssg As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ learn_vb.net/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+    Private Sub Btn_msg_Click(sender As Object, e As EventArgs) Handles btn_msg.Click
+
+        MessageBox.Show("hello")
+        MessageBox.Show("mina:")
+        MessageBox.Show("dawoud", "learn vb.net")
+
+
+        MsgBox("mm")
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sTrfirstname As String
+        Dim sTrlastname As String
+        sTrfirstname = "mina"
+        sTrlastname = "dawoud"
+
+        MsgBox("hello " + " , MR." + sTrfirstname & " " & sTrlastname + "  .... " + "how are you to day >>>")
+
+    End Sub
+
+    Private Sub Btn_datatype_Click(sender As Object, e As EventArgs) Handles btn_datatype.Click
+        Dim Crname As String
+        Dim CrModel As String
+        Dim CrDoor As Integer
+        Dim CrColour As String
+        Dim CrTaxed As Boolean
+        Dim CrsizCC As Integer
+        Dim CrPrice As Decimal
+        Dim CrDateRej As Date
+
+        Crname = "Ford"
+        CrModel = "Escort"
+        CrDoor = 5
+        CrColour = "red"
+        CrTaxed = True
+        CrsizCC = 1200
+        CrPrice = 999.99
+        CrDateRej = #01/09/2020#
+
+
+        MsgBox("Crname : " & Crname & vbNewLine & "CrModel:" & CrModel & vbNewLine & "CrDoor: " & CrDoor & vbNewLine & "CrColour: " & CrColour & vbNewLine & "CrTaxed : " & CrTaxed & vbNewLine & "CC:" & CrsizCC & vbNewLine & "Price :" & CrPrice & vbNewLine & "DateRej :" & CrDateRej)
+
+
+
+    End Sub
+
+    Private Sub Btn_mssg_Click(sender As Object, e As EventArgs) Handles btn_mssg.Click
+        Dim firstNam As String
+        Dim LastName As String
+        Dim Age As Integer
+        Dim listsbox As String
+
+        Age = InputBox("enter age ")
+        MsgBox(Age)
+
+        firstNam = TextBox1.Text
+
+        LastName = TextBox2.Text
+        Age = TextBox3.Text
+        'listsbox = ListBox1.SelectedItem
+        listsbox = ListBox1.SelectedIndex
+
+        MsgBox(firstNam & "" & LastName & "" & Age & "" & listsbox)
+
+
+#Region "mycomment"
+        'comment line1
+        'comment line2
+#End Region
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.Items.Add("hi")
+        ListBox1.Items.Add("my first")
+        ListBox1.Items.Add("project in vb ")
+    End Sub
+End Class
